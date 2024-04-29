0f29714c857580854db81c3cf1ed8d9dd50761eb
Fazalwahab12 <wahabdir567@gmail.com>
2024-04-29 10:03:04 -0700

    image pie chart

--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -7,7 +7,7 @@ const Tokenomics = () => {
       className="flex flex-col relative items-center justify-start  text-color-primary-color-2  "
     >
       <div className="mq450:pt-8 mq925:pt-32 text-61xl">
-        <h1 className="text-inherit font-bold font-inherit inline-block font-text-heading  mq450:text-5xl mq925:text-21xl">
+        <h1 className="text-inherit font-bold font-inherit inline-block font-text-heading  mq450:text-5xl mq925:text-10xl">
           <span>Token</span>
           <span className="text-gradient">omics</span>
         </h1>
@@ -33,39 +33,39 @@ const Tokenomics = () => {
           </div>
         </div>
       </div>
-
-      <div className="grid mq450:grid-cols-1 m-2 mq925:grid-cols-2 mq1350:grid-cols-3 mq1825:grid-cols-4 gap-8 pt-16 ">
-        <div className="w-[320px] h-[207px] text-center text-21xl text-color-text-color rounded-xl bg-mediumvioletred-200 border-solid border-gradient flex justify-center flex-col items-center font-text-body-2">
-          <h2 className="m-0 text-inherit leading-[150%] font-bold font-inherit inline-block w-[304px] z-[4] mq450:text-5xl mq450:leading-[36px] mq925:text-13xl mq925:leading-[48px] ">
-            Book Of Apes
-          </h2>
-          <b className="text-5xl inline-block w-[304px] z-[4] mq450:text-lgi">
-            Name
-          </b>
+      <div className=" flex pt-16 gap-16 mq450:flex-col mq925:flex-col   pb-5 ">
+        <div className="grid   px-10 mq450:grid-cols-1 m-2 mq925:grid-cols-2 mq1350:grid-cols-2 mq1825:grid-cols-2 gap-10 ">
+          <div className="w-[250px] h-[157px] text-center text-10xl text-color-text-color rounded-xl bg-mediumvioletred-200 border-solid border-gradient flex justify-center flex-col items-center font-text-body-2">
+            <h2 className="m-0 text-inherit leading-[150%] font-bold font-inherit inline-block w-[304px] mq450:text-5xl mq450:leading-[36px] mq925:text-13xl mq925:leading-[48px] ">
+              Book Of Apes
+            </h2>
+            <b className="text-3xl inline-block w-[304px] ">Name</b>
+          </div>
+          <div className="w-[250px] h-[157px] text-center text-10xl text-color-text-color rounded-xl bg-mediumvioletred-200 font-text-body-2 border-solid border-gradient flex justify-center flex-col items-center">
+            <h2 className="m-0 text-inherit leading-[150%] font-bold font-inherit inline-block w-[304px] mq450:text-5xl mq450:leading-[36px] mq925:text-13xl mq925:leading-[48px]">
+              SOLANA
+            </h2>
+            <b className="text-3xl inline-block w-[304px] ">Network</b>
+          </div>
+          <div className="w-[250px] h-[157px] text-center text-10xl text-color-text-color rounded-xl bg-mediumvioletred-200 border-solid font-text-body-2 border-gradient flex justify-center flex-col items-center">
+            <h2 className="m-0 text-inherit leading-[150%] font-bold font-inherit inline-block w-[304px] mq450:text-5xl mq450:leading-[36px] mq925:text-13xl mq925:leading-[48px]">
+              9
+            </h2>
+            <b className="text-3xl inline-block w-[304px] ">Decimal</b>
+          </div>
+          <div className="w-[250px] h-[157px] text-center text-10xl text-color-text-color rounded-xl bg-mediumvioletred-200 border-solid border-gradient font-text-body-2 flex justify-center flex-col items-center">
+            <h2 className="m-0 text-inherit leading-[150%] font-bold font-inherit inline-block w-[304px] mq450:text-5xl mq450:leading-[36px] mq925:text-13xl mq925:leading-[48px]">
+              420000000000
+            </h2>
+            <b className="text-3xl inline-block w-[304px] ">Total Supply</b>
+          </div>
         </div>
-        <div className="w-[320px] h-[207px] text-center text-21xl text-color-text-color rounded-xl bg-mediumvioletred-200 font-text-body-2 border-solid border-gradient flex justify-center flex-col items-center">
-          <h2 className="m-0 text-inherit leading-[150%] font-bold font-inherit inline-block w-[304px] z-[4] mq450:text-5xl mq450:leading-[36px] mq925:text-13xl mq925:leading-[48px]">
-            SOLANA
-          </h2>
-          <b className="text-5xl inline-block w-[304px] z-[4] mq450:text-lgi">
-            Network
-          </b>
-        </div>
-        <div className="w-[320px] h-[207px] text-center text-21xl text-color-text-color rounded-xl bg-mediumvioletred-200 border-solid font-text-body-2 border-gradient flex justify-center flex-col items-center">
-          <h2 className="m-0 text-inherit leading-[150%] font-bold font-inherit inline-block w-[304px] z-[4] mq450:text-5xl mq450:leading-[36px] mq925:text-13xl mq925:leading-[48px]">
-            9
-          </h2>
-          <b className="text-5xl inline-block w-[304px] z-[4] mq450:text-lgi">
-            Decimal
-          </b>
-        </div>
-        <div className="w-[320px] h-[207px] text-center text-21xl text-color-text-color rounded-xl bg-mediumvioletred-200 border-solid border-gradient font-text-body-2 flex justify-center flex-col items-center">
-          <h2 className="m-0 text-inherit leading-[150%] font-bold font-inherit inline-block w-[304px] z-[4] mq450:text-5xl mq450:leading-[36px] mq925:text-13xl mq925:leading-[48px]">
-            420000000000
-          </h2>
-          <b className="text-5xl inline-block w-[304px] z-[4] mq450:text-lgi">
-            Total Supply
-          </b>
+        <div className=" mq450:px-12 mq925:px-12  ">
+          <img
+            className="w-[80%] mq450:w-[100%] mq925:w-[100%] mq1825:pt-8 mq1350:pt-8 h-auto object-contain"
+            alt=""
+            src="/Pie Chart.png"
+          />
         </div>
       </div>
     </div>
